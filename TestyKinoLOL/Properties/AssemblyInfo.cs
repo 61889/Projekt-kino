677c5ec154aedb32484d2e3b221e11c71aa88d7f
Maksymilian <83972588+61889@users.noreply.github.com>
2021-06-29 22:01:03 +0200

    Add files via upload

---
+++ TestyKinoLOL/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestyKinoLOL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestyKinoLOL")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("419e76ff-4c5e-44c2-b424-83b106921c85")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestyKinoLOL/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using KinoRejestracja;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestyKinoLOL
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            var ob = new Form2("test");
+            //Act
+            var wynik = ob.funnyButtonsTest();
+            //Assert
+            Assert.AreEqual(wynik, 4);
+        }
+    }
+}
